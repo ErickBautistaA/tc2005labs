3a154ace8e9cb5f59d55221ad4117342bc09380a
Erick <erickeduardob@yahoo.com>
2024-08-15 20:58:25 -0600

    laboratorio8 terminado

---
+++ Laboratorio/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Sirve archivos estáticos desde la carpeta "public"
+app.use(express.static('public'));
+
+// Ruta para el archivo HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Inicia el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
